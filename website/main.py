8c8e643a0ed388ba634ede496311b148daec71bb
Aansh Kotian <76598799+Aone52@users.noreply.github.com>
2022-10-14 19:47:32 -0400

    something

--- website/main.py
+++ website/main.py
@@ -1,12 +1,25 @@
 
 import time
 
+from sqlalchemy import true
+
 from website import create_app
 
 
 
-app = create_app()
+# app = create_app()
 
-#only when you run the main.py script directly, will __name =='main'
-if __name__ == '__main__':
-    app.run(debug = True)
+# #only when you run the main.py script directly, will __name =='main'
+# if __name__ == '__main__':
+#     app.run(debug = True)
+
+def run():
+    app = create_app()
+
+    bool = True
+
+    if bool:
+        app.run(debug = True)
+
+
+run()
