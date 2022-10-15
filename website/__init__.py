138abe3d1301040ed80bf4f05890ceedccaeea57
Aansh Kotian <76598799+Aone52@users.noreply.github.com>
2022-10-14 21:22:10 -0400

    init_file modified

--- website/__init__.py
+++ website/__init__.py
@@ -1,1 +1,17 @@
+import time
 
+from sqlalchemy import true
+
+from website import create_app
+
+
+
+def run():
+    app = create_app()
+
+    bool = True
+
+    if bool:
+        app.run(debug = True)
+
+run()
